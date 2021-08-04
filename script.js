60c127c545fbce786e0efa9b29d7817a0e7a7917
Nembot Jules <85024341+NembotJules@users.noreply.github.com>
2021-08-04 11:33:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+/*<div class="notes">
+<div class="tools">
+    <button class="edit"> <i class="fas fa-edit"></i></button>
+    <button class="delete"><i class="fas fa-trash-alt"></i></button>
+</div>
+<div class="main hidden"> </div>
+<textarea></textarea>
+
+
+</div>*/
+const addBtn = document.getElementById('add'); 
+addBtn.addEventListener('click', ()=>{
+    addNewNote(); 
+})
+function addNewNote(){
+    const note = document.createElement('div'); 
+    note.classList.add('note'); 
+    note.innerHTML = ` <div class="notes">
+    <div class="tools">
+        <h4> NotesApp</h4>
+        <button class="edit"> <i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+    <div class="main hidden"></div>
+    <textarea placeholder="Enter your note here!"></textarea>
+    </div>`; 
+    
+const editBtn = note.querySelector('.edit'); 
+const deleteBtn = note.querySelector('.delete'); 
+const main = note.querySelector('.main'); 
+const textArea = note.querySelector('textarea'); 
+
+editBtn.addEventListener('click', ()=>{
+    main.classList.toggle('hidden'); 
+    textArea.classList.toggle('hidden'); 
+})
+deleteBtn.addEventListener('click', ()=>{
+    note.remove(); 
+})
+textArea.addEventListener('input', (e)=>{
+    const { value } = e.target; 
+    main.innerHTML = marked(value); 
+}); 
+
+
+
+    document.body.appendChild(note);
+}
